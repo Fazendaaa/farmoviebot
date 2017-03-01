3a53c900709711c98c821077369a7567260b7748
Farm <lucas.carotta@outlook.com>
2017-03-01 20:40:30 -0300

    Correcting help message.

--- bot/imdb.js
+++ bot/imdb.js
@@ -3,8 +3,8 @@ const Telegraf = require( 'telegraf' )
 const imdb = require( 'imdb-api' )
 const bot = new Telegraf( process.env.BOT_TOKEN )
 
-const welcome = "Welcome to IMDB bot. Type:\n/help"
-const help = "Usage:\n\n/search \'movie name\'\n/source -- see the code behind IMDBbot\n\nIt\'s only working in command so far, the next update it will have inline mode.\nAny bugs or suggestions, talk to: @farm_kun"
+const welcome = "Welcome to IMDB bot.\n\nType:\n/help"
+const help = "Usage:\n\n/search \'movie name\'\n/source -- see the code behind IMDBbot\n\nAny bugs or suggestions, talk to: @farm_kun"
 
 bot.command( 'start', (ctx) => {
 	console.log( 'start', ctx.from )

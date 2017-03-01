c01d29b965ad60668f914a936e6584cd05303b71
Farm <lucas.carotta@outlook.com>
2017-03-01 09:24:34 -0300

    Adding support commands.

--- bot/imdb.js
+++ bot/imdb.js
@@ -3,14 +3,29 @@ const Telegraf = require( 'telegraf' )
 const imdb = require( 'imdb-api' );
 
 const bot = new Telegraf( process.env.BOT_TOKEN )
+const welcome = 'Welcome to IMDB bot.\
+				\
+				Type:\
+					/help'
+const help = 'Usage:\
+				/search \'movie name\'\
+			\
+			It\'s only working in command so far, the next update it will\
+			have inline mode.\
+			\
+			Any bugs or suggestions, talk to: @farm_kun'
 
 bot.command('start', (ctx) => {
 	console.log( 'start', ctx.from )
-	ctx.reply( 'Welcome!' )
+	ctx.reply( welcome )
+})
+
+bot.command('help', (ctx) => {
+	console.log( 'help', ctx.from )
+	ctx.reply( help )
 })
 
 bot.command( 'search', (ctx) => {
-	console.log( 'serach', ctx.from )
 	const movie = ctx.message.text.split(' ').slice( 1 ).join(' ')
 
 	imdb.get( movie ).then( data => {

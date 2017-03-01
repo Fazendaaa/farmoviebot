8fad740f2295e273f8baf28d98f5806cece920a6
Farm <lucas.carotta@outlook.com>
2017-03-01 15:19:56 -0300

    Begining implementation on inline mode.

--- bot/imdb.js
+++ bot/imdb.js
@@ -1,37 +1,52 @@
 require( 'dotenv' ).config( { path: '../.env' } )
 const Telegraf = require( 'telegraf' )
-const imdb = require( 'imdb-api' );
-
+const imdb = require( 'imdb-api' )
 const bot = new Telegraf( process.env.BOT_TOKEN )
-const welcome = 'Welcome to IMDB bot.\
-				\
-				Type:\
-					/help'
-const help = 'Usage:\
-				/search \'movie name\'\
-			\
-			It\'s only working in command so far, the next update it will\
-			have inline mode.\
-			\
-			Any bugs or suggestions, talk to: @farm_kun'
 
-bot.command('start', (ctx) => {
+const welcome = "Welcome to IMDB bot. Type:\n/help"
+const help = "Usage:\n\t\t/search \'movie name\'\nIt\'s only working in command so far, the next update it will have inline mode.\nAny bugs or suggestions, talk to: @farm_kun"
+
+bot.command( 'start', (ctx) => {
 	console.log( 'start', ctx.from )
 	ctx.reply( welcome )
 })
 
-bot.command('help', (ctx) => {
+bot.command( 'help', (ctx) => {
 	console.log( 'help', ctx.from )
 	ctx.reply( help )
 })
 
+function search( movie, callback ) {
+	imdb.get( movie ).then( data => {
+		return callback( data )
+	} )
+}
+
 bot.command( 'search', (ctx) => {
 	const movie = ctx.message.text.split(' ').slice( 1 ).join(' ')
-
-	imdb.get( movie ).then( data => {
-		console.log( data ) 
-		ctx.reply( data.imdburl )
-	})
+	search( movie, function( response ) {
+  						ctx.reply( response.imdburl )
+  					}
+  		  )
 })
+
+/*
+bot.on( 'inline_query', (ctx) => {
+  const movie = ctx.inlineQuery.query || ''
+  search( movie, function( response ) {
+  						return ctx.answerInlineQuery( [
+	  						{
+						        id: response.name,
+						        title: response.name,
+						        type: 'article',
+						        input_message_content: {
+						        	message_text: response.imdburl
+						        }
+	  						}
+  						] )
+  					}
+  		  )
+} )
+*/
 
 bot.startPolling()

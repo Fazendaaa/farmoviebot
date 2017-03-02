9b2f29ae63236c20a958699bbcc862f62a088054
Farm <lucas.carotta@outlook.com>
2017-03-02 17:47:00 -0300

    Making the code more promise like.

--- bot/imdb.js
+++ bot/imdb.js
@@ -1,90 +1,73 @@
+/*
+	Helped me out:
+		*	http://stackoverflow.com/a/38987183/7092954
+*/
+
 require( 'dotenv' ).config( { path: '../.env' } )
+const Promise = require( 'promise' )
+
 const Telegraf = require( 'telegraf' )
 const imdb = require( 'imdb-api' )
 const bot = new Telegraf( process.env.BOT_TOKEN )
 
 const welcome = "Welcome to IMDB bot.\n\nType:\n/help"
-const help = "Usage:\n\n/search \'movie name\'\n/source -- see the code behind IMDBbot\n\nAny bugs or suggestions, talk to: @farm_kun"
+const help = "Usage:\n\n/search \'movie name\'\n/source -- see the code behind\
+			  IMDBbot\n\nAny bugs or suggestions, talk to: @farm_kun"
 
-bot.command( 'start', (ctx) => {
+bot.command( 'start', ctx => {
 	console.log( 'start', ctx.from )
 	ctx.reply( welcome )
 })
 
-bot.command( 'help', (ctx) => {
+bot.command( 'help', ctx => {
 	console.log( 'help', ctx.from )
 	ctx.reply( help )
 })
 
-function search( movie, callback ) {
-	const value = movie
-
-	imdb.get( value, ( err, response ) => {
-					if( err )
-						console.log( 'Error in function search: ', err )
-					else {
-						const data = response
-						return callback( data )
-					}
-				}
-			)
-}
-
-bot.command( 'search', (ctx) => {
+bot.command( 'search', ctx => {
 	const movie = ctx.message.text.split(' ').slice( 1 ).join(' ')
+	const movie = str_emoji.replace( new RegExp( /[\uD83C-\uDBFF\uDC00-\uDFFF]+/g ), '' );
+	console.log( movie )
 
-	search( movie, response => {
-						const data = response
-  						ctx.reply( data.imdburl )
-  					}
-  		  )
-})
+	imdb.get( movie ).then( response => ctx.reply( response.imdburl ) )
+	.catch( console.log( 'Reject promise in search function' ) )
+} )
 
-bot.command( 'source', (ctx) => {
+bot.command( 'source', ctx => {
 	ctx.reply( 'https://github.com/Fazendaaa/imdb_bot_telegram' )
 })
 
-const inline = [ {
-					id: 'search',
-					title: 'SEARCH IN IMDB',
-					type: 'article',
-					input_message_content: {
-	  					message_text: `Please, type again`
-				  	}
-				} ]
-
 function inline_search( movie, callback ) {
-	search( movie, response => {
-						const data = response
-						inline.push( data )
-						console.log( inline )
-						return callback( inline )
-			      }
-		  )
+	const inline = [ ]
+
+	imdb.get( movie )
+	.then( response => {
+			inline.push( {
+				id: response.title,
+				title: response.title,
+				type: 'article',
+				input_message_content: {
+					message_text: response.imdburl,
+					parse_mode: 'HTML'
+				},
+				url: response.url,
+				description: response.plot,
+				thumb_url: response.poster
+				}
+			)
+		callback( inline )
+	} )
+	.catch( console.log( 'Reject promise in inline search function' ) )
 }
 
-bot.on( 'inline_query', (ctx) => {
+bot.on( 'inline_query', ctx => {
 	const movie = ctx.inlineQuery.query || ''
 
 	inline_search( movie, response => {
-		const result = response.
-					   filter( ( value )=> value.title.toLowerCase().indexOf( movie.toLowerCase()) !== -1 ).
-					   map( ( value ) => {
-					       return {
-			    				id: value.title,
-			    				title: value.title,
-			    				type: 'article',
-			    				input_message_content: {
-			      					message_text: value.imdburl,
-			      					parse_mode: 'HTML'
-			    			  	},
-			    			  	url: value.url,
-			    			  	description: value.plot,
-			    			 	thumb_url: value.poster
-							}
-					   } )
-
-		return ctx.answerInlineQuery( result )
+		const result = response
+						.filter( value => value.title.toLowerCase()
+						.indexOf( movie.toLowerCase() ) !== -1 )
+		ctx.answerInlineQuery( result )
 	} )
 } )
 
